b522a0ad7f8529dc27ef0dab634d62ea7f338939
Azim0nt <kktotov457@gmail.com>
2024-03-30 21:19:40 +0500

    create dota comp

---
+++ frontend/src/components/Dota/index.jsx
@@ -0,0 +1,12 @@
+import './style.scss'
+function Dota() {
+    return ( 
+        <>
+        <div className="dota-wrapper">
+            <h1>Dota 2</h1>
+        </div>
+        </>
+     );
+}
+
+export default Dota;
