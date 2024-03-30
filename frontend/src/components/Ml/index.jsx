a994e64e97911b76702db73620980212f00c6c8f
Azim0nt <kktotov457@gmail.com>
2024-03-30 21:20:35 +0500

    create ml comp

---
+++ frontend/src/components/Ml/index.jsx
@@ -0,0 +1,12 @@
+import './style.scss';
+function Ml() {
+    return ( 
+        <>
+            <div className="ml-wrapper">
+                <h1>Mobile Legends</h1>
+            </div>
+        </>
+     );
+}
+
+export default Ml;
