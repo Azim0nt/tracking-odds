e926564021c12635c55820809edb402eaee4bbcc
Azim0nt <kktotov457@gmail.com>
2024-03-30 21:20:03 +0500

    create lol comp

---
+++ frontend/src/components/Lol/index.jsx
@@ -0,0 +1,12 @@
+import './style.scss'
+function Lol() {
+    return ( 
+        <>
+        <div className="lol-wrapper">
+            <h1>League of Legends</h1>
+        </div>
+        </>
+     );
+}
+
+export default Lol;
